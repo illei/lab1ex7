e303728ce927658a94583501eaea1a2327765f9c
Illei Adrian <illei.adrian@yahoo.com>
2023-09-06 11:29:13 +0200

    Add project files.

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab1ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+            impar
+             */
+            int numar;
+            numar = int.Parse(Console.ReadLine());
+            if (numar % 2 == 0)
+            {
+                Console.WriteLine("par");
+            }
+            else
+            {
+                Console.WriteLine("impar");
+            }
+        }
+    }
+}
